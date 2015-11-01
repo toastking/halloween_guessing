77f1b44d04704a69c05cfe3e1534d41b5c20f173
Matt Del Signore <mattdelsig@me.com>
2015-11-01 13:52:26 -0500

    gitignore file

--- horrorguessing/main.py
+++ horrorguessing/main.py
@@ -3,19 +3,23 @@ from flask import request
 from flask import render_template
 import json
 import random
+import os
+
 app = Flask(__name__)
 
 data = [] # the json dictionary we will use for our data
 filepath = "./data.json" #directory of the json file 
 
-@app.route('/', methods = ['GET','POST'])
+# get the json data
+json_file = open(filepath)
+data = json.load(json_file)["data"]
+json_file.close()
+
+
+@app.route('/')
 def index():
 	i = random.randint(0,len(data) - 1) #generate a random data
  	return render_template('index.html', title = data[i]['title'], type = data[i]['type'], desc = data[i]['description'], url = data[i]['url'])
 
 if __name__ == '__main__':
-	# get the json data
-	json_file = open(filepath)
-	data = json.load(json_file)["data"]
-	json_file.close()
 	app.run(debug = True)
